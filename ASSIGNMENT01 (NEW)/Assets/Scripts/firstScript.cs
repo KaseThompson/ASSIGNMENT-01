fcf6b19364ceab3c15d28b75e622216780e17b8c
FSD38\Kase.thompson <Kase.thompson@gm.fsd38.ab.ca>
2020-09-08 10:44:34 -0600

    initial commit

---
+++ ASSIGNMENT01 (NEW)/Assets/Scripts/firstScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World!");
+
+
+
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
